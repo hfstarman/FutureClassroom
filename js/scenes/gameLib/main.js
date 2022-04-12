12233b6514d70ebfa82c672d200857f1cbbdd993
hfstarman <hfstarman@gmail.com>
2022-04-12 15:10:06 -0400

    enemies walk at their max speed

--- js/scenes/gameLib/main.js
+++ js/scenes/gameLib/main.js
@@ -2,12 +2,12 @@
 import { handleControllerActions } from "./controllers.js";
 import { handleEnemyMovement } from "./enemyInteractions.js";
 import { handleObjectMovement, setObjectColors} from "./objectInteractions.js";
+import { handleCollisions } from "./collisions.js";
 
 export default function runGame() {
   setObjectColors();
+  handleCollisions();
   handleEnemyMovement();
   handleObjectMovement();
   handleControllerActions();
 }
-
-// handleCollisions();
